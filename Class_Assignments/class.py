103bf69670aa06460e417c930888de30556723be
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-07-24 16:12:02 -0500

    Class Creation with Inheritance

---
+++ Class_Assignments/class.py
@@ -0,0 +1,15 @@
+
+
+class Car:
+    make = ' '
+    model = ' '
+    color = ' '
+
+class ev(Car):
+    battery_size = ' '
+    quick_charge = True
+
+class ice(Car):
+    fuel_type = ' '
+    num_of_cylinder = 0
+    
