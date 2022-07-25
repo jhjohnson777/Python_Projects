ccb4909f217da90a34f9dd1390e6dcde7b74136f
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-07-25 11:15:04 -0500

    Added comments

--- Class_Assignments/class.py
+++ Class_Assignments/class.py
@@ -1,14 +1,16 @@
 
-
+# parent class
 class Car:
     make = ' '
     model = ' '
     color = ' '
 
+# child class 1
 class ev(Car):
     battery_size = ' '
     quick_charge = True
 
+#child class 2
 class ice(Car):
     fuel_type = ' '
     num_of_cylinder = 0
