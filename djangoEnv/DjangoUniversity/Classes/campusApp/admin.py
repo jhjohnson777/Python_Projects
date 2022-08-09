0120c5ad6e19ade28023fdd7025430563f4d2b89
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-08-08 20:01:01 -0500

    Checkbook assignment

--- djangoEnv/DjangoUniversity/Classes/campusApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UniversityCampus #imports UniversityCampus from models in the same app
-
-# Register your models here.
-admin.site.register(UniversityCampus) #Registers class
